b0c826a2549b2604cf16cfea3af93a4cf4effa27
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-11-09 18:20:36 +0100

    les routes dans app.js

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const xss = require('xss-clean');
 const helmet = require("helmet");
 const app = express();
+const routesPosts = require('./routes/routesPosts');
+const routesUsers = require('./routes/routesUsers');
+const routesMod = require('./routes/routesModera');
 
-
-
-
+app.use(xss());
+app.use (helmet()); 
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,11 +17,34 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.json());
-
-
-
-
-
+app.use('/api/posts', routesPosts);
+app.use('/api/auth', routesUsers);
+app.use('/api/moderation', routesModera);
 
 module.exports = app;
+
+
+// const express = require('express');
+// const xss = require('xss-clean');
+// const helmet = require("helmet");
+// const app = express();
+
+
+
+
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// });
+
+// app.use(express.json());
+
+
+
+
+
+
+// module.exports = app;
