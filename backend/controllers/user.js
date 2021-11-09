f04f38563bb7c5263317bc3e226d5dd8047916b9
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-11-09 18:08:05 +0100

    user.js suite dans controllers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -39,3 +39,52 @@ exports.login = (req, res, next) => {
             res.status(400).json(error)
         })
 }
+
+exports.seeMyProfile = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const userId = decodedToken.userId;
+    let sqlInserts = [userId];
+    userModels.seeMyProfile(sqlInserts)
+        .then((response) =>{
+            res.status(200).json(JSON.stringify(response))
+        })
+        .catch((error) =>{
+            console.log(error);
+            res.status(400).json(error)
+        })
+}   
+exports.updateUser = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const userId = decodedToken.userId;
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+    let sqlInserts = [firstName, lastName, email, userId];
+    userModels.updateUser(sqlInserts)
+        .then((response) =>{
+            res.status(200).json(JSON.stringify(response))
+        })
+        .catch((error) =>{
+            res.status(400).json(error)
+        })
+}
+ 
+exports.deleteUser = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const userId = decodedToken.userId;
+    let sqlInserts = [userId];
+    userModels.deleteUser(sqlInserts)
+        .then((response) =>{
+            res.status(200).json(JSON.stringify(response))
+        })
+        .catch((error) =>{
+            console.log(error);
+            res.status(400).json(error)
+        })
+} 
+ 
+
+
