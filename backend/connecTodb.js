fed5efcccd825b5ce4582df79fc27a6ccf24cc9d
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-11-09 17:48:58 +0100

    La Database

---
+++ backend/connecTodb.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+console.log('Connexion à la base de données...');
+let connecTodb = mysql.createConnection({ 
+    host: 'localhost', 
+    user: 'root', 
+    password: 'tojil1908', 
+    database: 'groupomania_le_reseau_db', 
+});
+connectdb.connect(function(err) { 
+    if (err) throw err;
+    console.log('Connecté!')
+});
+
+module.exports = connecTodb;
