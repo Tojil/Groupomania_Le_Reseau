2cc3929c7fe2e9d9db19630b576db52a03a61654
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-11-16 15:13:01 +0100

    Changement de titre

---
+++ frontend/src/components/Accueil.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-app id="accueil">
+        <top-header/>
+        <router-view></router-view>
+
+        <v-carousel
+            id="carousel"
+            cycle
+            height="auto"
+            hide-delimiters
+            :show-arrows="false"
+            >
+            <v-carousel-item
+              v-for="(item,i) in items"
+              :key="i"
+              :src="item.src"
+            ></v-carousel-item>
+        </v-carousel>      
+
+    </v-app>
+</template>
+<script>
+import Header from "./Hall/Header"
+
+export default {
+    name: "Accueil",
+    data(){
+        return{
+            items: [
+            {
+                src: require("../assets/images/immeuble.jpg"),
+            },
+            {
+                src: require("../assets/images/openspace.jpg"),
+            },
+            {
+                src: require("../assets/images/chocolat.jpg"),
+            },
+            ]
+            
+        }
+    },
+    components: {
+        "top-header": Header,
+    },
+}
+</script>
+<style lang="scss">
+    #accueil{
+        background: no-repeat center top/20% url("../assets/icon-above-font.png");
+        display: flex;
+        flex-direction: row;
+    }
+    #carousel{
+        width: 60%;
+        margin-top: 17%;
+        margin-left: 20%;
+    }
+    .header{
+        background-color: rgb(253,45,1);
+        color: white;
+    }
+    @media screen and (min-width: 200px) and (max-width: 640px) {
+    #carousel {
+    padding-right: 15%;
+    width: 80%;
+    } 
+}
+        
+    
+</style>
